04e230f9c54dc33f9ededdab04c24046848a375b
Anorexorcist <deniscernat@gmail.com>
2021-01-27 18:27:00 +0200

    first commit

---
+++ src/test/java/jdc/spring5dev/spring5jokesapp/Spring5JokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package jdc.spring5dev.spring5jokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5JokesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
